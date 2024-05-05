ca5cda05609566b2fb967439f43dc611da2cdb22
Ben Kroul <benkroul@DNa811f22.SUNet>
2024-05-04 22:53:30 -0700

    starting

---
+++ limit_order_book.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class OrderBook():
+    def __init__(self):
+        pass
+
+    # plot the buy, sell thingies
+    def plot(self):
+        pass
--- util.py
+++ util.py
@@ -2,3 +2,4 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # insert util stuff
+

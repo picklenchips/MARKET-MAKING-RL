6663c19b397b9162df6f103cfe41105a5d5f031d
Ben Kroul <benkroul@DN0a1f6146.SUNet>
2024-05-05 01:18:07 -0700

    order_has_been_booked

--- limit_order_book.py
+++ limit_order_book.py
@@ -1,29 +1,146 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from cycler import cycler
+#           |   Red  |   Blue  |  Orange |  Purple | Yellow  |   Green |   Teal  | Grey
+hexcolors = ['DC267F', '648FFF', 'FE6100', '785EF0', 'FFB000', '009E73', '3DDBD9', '808080']
+mpl.rcParams['axes.prop_cycle'] = cycler('color', [mpl.colors.to_rgba('#' + c) for c in hexcolors])
 
-import queue  # priority queue
+import heapq  # priority queue
+
+from util import uFormat
+
+class LimitOrder():
+    def __init__(self, nstocks, price):
+        self.n = nstocks
+        self.p = price
+
+# assume midprice is just average between two sell orders? 
+# that is, not weighted by number of things in each order
+# assume that limit orders can only be bought and sold by market orders
+# that is, we ignore the case that a buy and sell limit order have the same 
+# exact price
 
 class OrderBook():
     def __init__(self):
-        self.buys = queue.PriorityQueue()
-        self.sells = queue.PriorityQueue()
-        self.midprice = 0
-        self.spread = 0
+        # keep track of limit orders
+        self.buys = []  # 
+        self.sells = []
+        self.lowest_sell = np.inf
+        self.highest_buy = 0
+        # and their relevant pricing dynamics
+        self.midprice = None
+        self.spread = None
     
-    def market_buy(self, nstocks):
-        """Buy (up to) nstocks limit-sell orders"""
-        pass
+    def recalculate(self):
+        if len(self.sells) and len(self.buys):
+            lowest_sell = self.sells[0][0]
+            highest_buy = -self.buys[0][0]
+            self.midprice = (lowest_sell+highest_buy)/2
+            self.spread = lowest_sell-highest_buy
+            if self.spread < 0:
+                print("ERROR: unrealistic spread!!")
 
-    def market_sell(self, nstocks):
-        """Sell (up to) nstocks limit-buy orders"""
-        pass
+    def market_buy(self, nstocks, maxprice=None):
+        """Buy (up to) nstocks stocks to lowest-priced limit-sell orders
+        returns list of (price, n_stocks) orders that have been bought
+        optionally, only buy stocks valued below max price"""
+        #TODO: maybe delete n_sold tracking
+        # assume you can buy a portion of a limit order and the rest stays?
+        bought = []; n_bought = 0; do_update = False
+        while nstocks > 0 and len(self.sells):
+            if maxprice:  # only buy stocks less than max price
+                if self.sells[0][0] > maxprice: break
+            price, n = heapq.heappop(self.sells)
+            # buy up to nstocks stocks from the n available at this price
+            n_bought += min(n, nstocks)
+            bought.append((price, min(n, nstocks)))
+            # place remaining portion of limit order back
+            if nstocks < n: 
+                heapq.heappush(self.sells,(price, n-nstocks))
+            else: 
+                do_update = True
+            nstocks -= n
+        if do_update: self.recalculate()
+        return n_bought, bought
 
-    def limit_buy(self):
-        pass
+    def market_sell(self, nstocks, minprice=None):
+        """Sell (up to) nstocks stocks to highest-priced limit-buy orders
+        optionally, only sell stocks valued above min price"""
+        #TODO: maybe delete n_sold tracking?
+        sold = []; n_sold = 0; do_update = False
+        while nstocks > 0:
+            if minprice:  # only sell stocks greater than min price
+                if self.buys[0][0] < minprice: break
+            price, n = heapq.heappop(self.buys)
+            price = -price  # convert back to normal price
+            # sell up to nstocks stocks to the n available at this price
+            n_sold += min(n, nstocks)
+            sold.append((price,min(n, nstocks)))
+            # place remaining portion of limit order back
+            if nstocks < n: 
+                heapq.heappush(self.buys,(-price, n-nstocks))
+            else: 
+                do_update = True
+            nstocks -= n
+        if do_update: self.recalculate()
+        return n_sold, sold
 
-    def limit_sell(self):
-        pass
+    def limit_buy(self, nstocks, price):
+        """Add a limit-buy order. Sorted highest-to-lowest"""
+        # buying higher than lowest sell -> market buy instead
+        if len(self.sells):
+            if price >= self.sells[0][0]:
+                print("hold up, eating rn...")
+                nbought, bought = self.market_buy(nstocks, maxprice=price)
+                nstocks -= nbought
+                if nstocks == 0: return  # all eaten!
+        heapq.heappush(self.buys, (-price, nstocks))
+        # is now highest buy order, recalculate
+        if -price == self.buys[0][0]:
+            self.recalculate()
 
-    # plot the buy, sell thingies
+    def limit_sell(self, nstocks, price):
+        """Add a limit-sell order"""
+        # selling lower than highest buy order -> sell some now!
+        if len(self.buys):
+            if price <= -self.buys[0][0]:
+                print("hold up, eating rn...")
+                nsold, sold = self.market_sell(nstocks, minprice=price)
+                nstocks -= nsold
+                if nstocks == 0: return  # all eaten!
+        heapq.heappush(self.sells, (price, nstocks))
+        # is now lowest sell order, recalculate
+        if price == self.sells[0][0]:
+            self.recalculate()
+    
     def plot(self):
-        pass
+        """Make histogram of current limit-orders"""
+        fig, ax = plt.subplots()
+        ax.set(xlabel='Price per Share',ylabel='Volume')
+        ax.bar([-b[0] for b in self.buys],[b[1] for b in self.buys],label='Buy Orders')
+        ax.bar([s[0] for s in self.sells],[s[1] for s in self.sells],label='Sell Orders')
+        lowx, highx = ax.get_ylim()
+        ax.plot([self.midprice,self.midprice],[lowx,highx*0.8],linestyle='dashed',color='black')
+        plt.title(f"Order Book. midprice = {uFormat(self.midprice,0)}, spread = {uFormat(self.spread,0)}")
+        plt.legend()
+        plt.show()
+
+N = 10
+mean_buy = 100
+mean_sell = 150
+std_buy = 30
+std_sell = 30
+buy_orders = np.round(np.random.normal(size=(N,))*std_buy + mean_buy)
+sell_orders = np.round(np.random.normal(size=(N,))*std_sell + mean_sell)
+
+book = OrderBook()
+# alternate buy, sell orders
+for i in range(N):
+    n = np.random.randint(1,10)
+    book.limit_buy(n,buy_orders[i])
+    print(f'bought {n} stonks at {buy_orders[i]}. ',end='')
+    n = np.random.randint(1,10)
+    book.limit_sell(n,sell_orders[i])
+    print(f'sold {n} stonks at {sell_orders[i]}')
+    book.plot()
--- util.py
+++ util.py
@@ -1,6 +1,230 @@
 import numpy as np
+from glob import glob
+from cycler import cycler
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import sys, time, os
+from scipy.optimize import curve_fit
+from tqdm import tqdm
 
-# insert util stuff
-# make into plotting?
-# idk
+#           |   Red  |   Blue  |  Orange |  Purple | Yellow  |   Green |   Teal  | Grey
+hexcolors = ['DC267F', '648FFF', 'FE6100', '785EF0', 'FFB000', '009E73', '3DDBD9', '808080']
+mpl.rcParams['axes.prop_cycle'] = cycler('color', [mpl.colors.to_rgba('#' + c) for c in hexcolors])
+
+SAVE_DIR = os.getcwd()+"/"
+SAVE_EXT = ".png"
+np.set_printoptions(precision=4)
+
+def uFormat(number, uncertainty, round = 0, sig_figs = 4, FormatDecimals = False):
+    """
+    Returns "num_rounded(with_sgnfcnt_dgts_ofuncrtnty)", formatted to 10^round
+    According to section 5.3 of "https://pdg.lbl.gov/2011/reviews/rpp2011-rev-rpp-intro.pdf"
+
+    Arguments:
+    - float number:      the value
+    - float uncertainty: the absolute uncertainty (stddev) in the value
+       - if zero, will format number to optional number of sig_figs (see sig_figs)
+    - int round:  optionally, shift the resultant number to a higher/lower digit expression
+       - i.e. if number is in Hz and you want a string in GHz, specify round = 9
+               likewise for going from MHz to Hz, specify round = -6
+    - int sig_figs: when uncertainty = 0, format number to degree of sig figs instead
+       - if zero, will simply return number as string
+    - bool FormatDecimals:  for a number 0.00X < 1e-2, option to express in "X.XXe-D" format
+             for conciseness. doesnt work in math mode because '-' is taken as minus sign
+    """
+    num = str(number); err = str(uncertainty)
+    
+    sigFigsMode = not uncertainty    # UNCERTAINTY ZERO: IN SIG FIGS MODE
+    if sigFigsMode and not sig_figs: # nothing to format
+        return num
+    
+    negative = False  # add back negative later
+    if num[0] == '-':
+        num = num[1:]
+        negative = True
+    if err[0] == '-':
+        err = err[1:]
+    
+    # ni = NUM DIGITS to the RIGHT of DECIMAL
+    # 0.00001234=1.234e-4 has ni = 8, 4 digs after decimal and 4 sig figs
+    # 1234 w/ ni=5 corresponds to 0.01234
+    ni = ei = 0  
+    if 'e' in num:
+        ff = num.split('e')
+        num = ff[0]
+        ni = -int(ff[1])
+    if 'e' in err:
+        ff = err.split('e')
+        err = ff[0]
+        ei = -int(ff[1])
+
+    if not num[0].isdigit():
+        print("uFormat: {num} isn't a number")
+        return num
+    if not err[0].isdigit():
+        err = '?'
+
+    # comb through error, get three most significant figs
+    foundSig = False; decimal = False
+    topThree = ""; numFound = 0
+    jErr = ""
+    for ch in err:
+        if decimal:
+            ei += 1
+        if not foundSig and ch == '0': # dont care ab leading zeroes
+            continue  
+        if ch == '.':
+            decimal = True
+            continue
+        jErr += ch
+        if numFound >= 3:  # get place only to three sigfigs
+            ei -= 1
+            continue
+        foundSig = True
+        topThree += ch
+        numFound += 1
+    
+    foundSig = False; decimal = False
+    jNum = ""
+    for ch in num:
+        if decimal:
+            ni += 1
+        if not foundSig and ch == '0': # dont care ab leading zeroes
+            continue  
+        if ch == '.':
+            decimal = True
+            continue
+        jNum += ch
+        foundSig = True
+    
+    # round error correctly according to PDG
+    if len(topThree) == 3:
+        nTop = int(topThree)
+        if nTop < 355: # 123 -> (12.)
+            Err = int(topThree[:2])
+            if int(topThree[2]) >= 5:
+                Err += 1
+            ei -= 1
+        elif nTop > 949: # 950 -> (10..)
+            Err = 10
+            ei -= 2
+        else: # 355 -> (4..)
+            Err = int(topThree[0])
+            if int(topThree[1]) >= 5:
+                Err += 1
+            ei -= 2
+        Err = str(Err)
+    else:
+        Err = topThree
+
+    n = len(jNum); m = len(Err)
+    nBefore = ni - n  #; print(num, jNum, n, ni, nBefore)
+    eBefore = ei - m  #; print(err, Err, m, ei, eBefore)
+    if nBefore > eBefore:  # uncertainty is a magnitude larger than number, still format number
+        if not sigFigsMode:
+            print(f'Uncrtnty: {uncertainty} IS MAGNITUDE(S) > THAN Numba: {number}')
+        Err = '?'
+    if sigFigsMode or nBefore > eBefore:
+        ei = nBefore + sig_figs
+
+    # round number to error
+    d = ni - ei 
+    if ni == ei: 
+        Num = jNum[:n-d]
+    elif d > 0:  # error has smaller digits than number = round number
+        Num = int(jNum[:n-d])
+        if int(jNum[n-d]) >= 5:
+            Num += 1
+        Num = str(Num)
+    else:  # error << num
+        Num = jNum
+        if ei < m + ni:
+            Err = Err[n+d-1]
+        else:
+            Err = '0'
+    if ni >= ei: ni = ei  # indicate number has been rounded
+    
+    n = len(Num)
+    # if were at <= e-3 == 0.009, save formatting space by removing decimal zeroes
+    extraDigs = 0
+    if not round and FormatDecimals and (ni-n) >= 2:
+        round -= ni - n + 1
+        extraDigs = ni - n + 1
+    
+    # shift digits up/down by round argument
+    ni += round
+    end = ''
+    if ni >= n:   # place decimal before any digits
+        Num = '0.' + "0"*(ni-n) + Num
+    elif ni > 0:  # place decimal in-between digits
+        Num = Num[:n-ni] + '.' + Num[n-ni:]
+    elif ni < 0:  # add non-significant zeroes after number
+        end = 'e'+str(-ni)
+    if extraDigs:  # format removed decimal zeroes
+        end = 'e'+str(-extraDigs)
+    
+    if negative: Num = '-' + Num  # add back negative
+    if not sigFigsMode:
+        end = '(' + Err + ')' + end
+    return Num + end
+
+def timeIt(func):
+    """@ timeIt: Wrapper to print run time of a function."""
+    def wrapper(*args, **kwargs):
+        start_time = time.clock_gettime_ns(0)
+        res = func(*args, **kwargs)
+        end_time = time.clock_gettime_ns(0)
+        diff = (end_time - start_time) * 10**(-9)
+        print(func.__name__, 'ran in %.6fs' % diff)
+        return res
+    return wrapper
+
+def binarySearch(X_val, X, decreasing=False):
+    """
+    For sorted X, returns index i such that X[:i] < X_val, X[i:] >= X_val
+     - if decreasing,returns i such that    X[:i] > X_val, X[i:] <= X_val
+    """
+    l = 0; r = len(X) - 1
+    #print(f"searching for {X_val}, negative={negative}")
+    m = (l + r) // 2
+    while r > l:  # common binary search
+        #print(f"{l}:{r} is {X[l:r+1]}, middle {X[m]}")
+        if X[m] == X_val:  # repeat elements of X_val in array
+            break
+        if decreasing: # left is always larger than right
+            if X[m] > X_val:
+                l = m + 1
+            else:
+                r = m - 1
+        else:        # right is always larger than left
+            if X[m] < X_val:
+                l = m + 1
+            else:
+                r = m - 1
+        m = (l + r) // 2
+    if r < l:
+        return l
+    if m + 1 < len(X):  # make sure we are always on right side of X_val
+        if X[m] < X_val and not decreasing:
+            return m + 1
+        if X[m] > X_val and decreasing:
+            return m + 1
+    if X[m] == X_val:  # repeat elements of X_val in array
+        if decreasing:
+            while m > 0 and X[m - 1] == X_val:
+                m -= 1
+        elif not decreasing:
+            while m + 1 < len(X) and X[m + 1] == X_val:
+                m += 1
+    return m
+
+# linear interpolate 1D with sorted X
+def linearInterpolate(x,X,Y):
+    """example: 2D linear interpolate by adding interpolations from both
+    - """
+    i = binarySearch(x,X)
+    if i == 0: i += 1  # lowest ting, interpolate backwards
+    m = (Y[i]-Y[i-1])/(X[i]-X[i-1])
+    b = Y[i] - m*X[i]
+    return m*x + b

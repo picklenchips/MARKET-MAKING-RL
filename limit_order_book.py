9f3927026ac117da3631aef320ea72f4ac175a54
Ben Kroul <benkroul@DN0a1f6146.SUNet>
2024-05-04 23:34:22 -0700

    esketit

--- limit_order_book.py
+++ limit_order_book.py
@@ -1,8 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import queue  # priority queue
+
 class OrderBook():
     def __init__(self):
+        self.buys = queue.PriorityQueue()
+        self.sells = queue.PriorityQueue()
+        self.midprice = 0
+        self.spread = 0
+    
+    def market_buy(self, n_stocks):
+        pass
+
+    def market_sell(self):
+        pass
+
+    def limit_buy(self):
+        pass
+
+    def limit_sell(self):
         pass
 
     # plot the buy, sell thingies
--- util.py
+++ util.py
@@ -2,4 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # insert util stuff
-
+# make into plotting?
+# idk

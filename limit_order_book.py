4879c169ce9332fe6ab853afe4d1c0b347ef567f
Ben Kroul <benkroul@DN0a1f6146.SUNet>
2024-05-05 15:44:43 -0700

    order_book_done_next

--- limit_order_book.py
+++ limit_order_book.py
@@ -32,14 +32,15 @@ class OrderBook():
     - buy: market-buy # of stocks cheaper than some maximum price
     - sell: market-sell # of stocks more expensive than some minimum price 
     - bid: create a limit order to buy # stocks at some price
+      - stored self.bids as (-price, #)
     - ask: create a limit order to sell # stocks at some price 
+      - stored self.asks as (price, #)
+    and two states self.midprice, self.spread
     """
     def __init__(self):
         # keep track of limit orders
-        self.buys = []
-        self.sells = []
-        self.lowest_sell = np.inf
-        self.highest_buy = 0
+        self.bids = []
+        self.asks = []
         # and their relevant pricing dynamics
         self.midprice = None
         self.spread = None
@@ -47,17 +48,17 @@ class OrderBook():
     def recalculate(self):
         """ Recalculate self.midprice and self.spread """
         self.spread = self.midprice = None 
-        if len(self.sells):
-            self.midprice = self.sells[0][0]
-            if len(self.buys):
-                lowest_sell = self.sells[0][0]
-                highest_buy = -self.buys[0][0]
+        if len(self.asks):
+            self.midprice = self.asks[0][0]
+            if len(self.bids):
+                lowest_sell = self.asks[0][0]
+                highest_buy = -self.bids[0][0]
                 self.midprice = (lowest_sell+highest_buy)/2
                 self.spread = lowest_sell-highest_buy
                 if self.spread < 0:
                     print("ERROR: unrealistic spread!!")
-        elif len(self.buys):
-            self.midprice = -self.buys[0][0]
+        elif len(self.bids):
+            self.midprice = -self.bids[0][0]
 
     def buy(self, nstocks, maxprice=None):
         """Buy (up to) nstocks stocks to lowest-priced limit-sell orders
@@ -65,16 +66,16 @@ class OrderBook():
                          list of (price, n_stocks) orders that have been bought
         optionally, only buy stocks valued below max price"""
         bought = []; n_bought = 0; do_update = False
-        while nstocks > 0 and len(self.sells):
+        while nstocks > 0 and len(self.asks):
             if maxprice:  # only buy stocks less than max price
-                if self.sells[0][0] > maxprice: break
+                if self.asks[0][0] > maxprice: break
             # buy up to nstocks stocks from the n available at this price
-            price, n = heapq.heappop(self.sells)
+            price, n = heapq.heappop(self.asks)
             n_bought += min(n, nstocks)
             bought.append((price, min(n, nstocks)))
             # place remaining portion of limit order back
             if nstocks < n: 
-                heapq.heappush(self.sells, (price, n-nstocks))
+                heapq.heappush(self.asks, (price, n-nstocks))
             else: 
                 do_update = True
             nstocks -= n
@@ -85,17 +86,17 @@ class OrderBook():
         """Sell (up to) nstocks stocks to highest-priced limit-buy orders
         optionally, only sell stocks valued above min price"""
         sold = []; n_sold = 0; do_update = False
-        while nstocks > 0 and len(self.buys):
+        while nstocks > 0 and len(self.bids):
             if minprice:  # only sell stocks greater than min price
-                if -self.buys[0][0] < minprice: break
-            price, n = heapq.heappop(self.buys)
+                if -self.bids[0][0] < minprice: break
+            price, n = heapq.heappop(self.bids)
             price = -price  # convert back to normal price
             # sell up to nstocks stocks to the n available at this price
             n_sold += min(n, nstocks)
             sold.append((price,min(n, nstocks)))
             # place remaining portion of limit order back
             if nstocks < n: 
-                heapq.heappush(self.buys,(-price, n-nstocks))
+                heapq.heappush(self.bids,(-price, n-nstocks))
             else: 
                 do_update = True
             nstocks -= n
@@ -105,61 +106,68 @@ class OrderBook():
     def bid(self, nstocks, price):
         """Add a limit-buy order. Sorted highest-to-lowest"""
         # buying higher than lowest sell -> market buy instead
-        if len(self.sells):
-            if price >= self.sells[0][0]:
-                nbought, bought = self.market_buy(nstocks, maxprice=price)
+        if len(self.asks):
+            if price >= self.asks[0][0]:
+                nbought, bought = self.buy(nstocks, maxprice=price)
                 nstocks -= nbought
                 if nstocks == 0: return  # all eaten!
-        heapq.heappush(self.buys, (-price, nstocks))
+        heapq.heappush(self.bids, (-price, nstocks))
         # is now highest buy order, recalculate
-        if -price == self.buys[0][0]:
+        if -price == self.bids[0][0]:
             self.recalculate()
 
     def ask(self, nstocks, price):
         """Add a limit-sell order"""
         # selling lower than highest buy order -> sell some now!
-        if len(self.buys):
-            if price <= -self.buys[0][0]:
-                nsold, sold = self.market_sell(nstocks, minprice=price)
+        if len(self.bids):
+            if price <= -self.bids[0][0]:
+                nsold, sold = self.sell(nstocks, minprice=price)
                 nstocks -= nsold
                 if nstocks == 0: return  # all eaten!
-        heapq.heappush(self.sells, (price, nstocks))
+        heapq.heappush(self.asks, (price, nstocks))
         # is now lowest sell order, recalculate
-        if price == self.sells[0][0]:
+        if price == self.asks[0][0]:
             self.recalculate()
     
     def plot(self):
         """Make histogram of current limit-orders"""
         fig, ax = plt.subplots()
         ax.set(xlabel='Price per Share',ylabel='Volume')
-        ax.bar([-b[0] for b in self.buys],[b[1] for b in self.buys],label='Buy Orders',edgecolor='black',linewidth=0.5)
-        ax.bar([s[0] for s in self.sells],[s[1] for s in self.sells],label='Sell Orders',edgecolor='black',linewidth=0.5)
-        if self.midprice:
+        ax.bar([-b[0] for b in self.bids],[b[1] for b in self.bids],label='Bids',edgecolor='black',linewidth=0.5)
+        ax.bar([s[0] for s in self.asks],[s[1] for s in self.asks],label='Asks',edgecolor='black',linewidth=0.5)
+        title = "Order Book"
+        if self.midprice:  # add dashed line for midprice
             lowx, highx = ax.get_ylim()
             ax.plot([self.midprice,self.midprice],[lowx,highx*0.8],linestyle='dashed',color='black')
-        plt.title(f"Order Book. midprice = {uFormat(self.midprice,0)}, spread = {uFormat(self.spread,0)}")
-        plt.legend()
+            title += f" - midprice = {uFormat(self.midprice,0)}, spread = {uFormat(self.spread,0)}"
+        plt.title(title)
+        plt.legend(loc='upper center',ncol=2)
         plt.show()
 
 # we getting Pythonic up in this
 if __name__ == "__main__":
     # test orderbook eating properties
-    N = 10
-    mean_buy = 100
-    mean_sell = 100
-    std_buy = 30
-    std_sell = 30
-    buy_orders = (np.random.normal(size=(N,))*std_buy + mean_buy).astype(int)
-    sell_orders = (np.random.normal(size=(N,))*std_sell + mean_sell).astype(int)
+    while 1:
+        N = input("number of buy-sell iterations:\n>> ").strip()
+        if N.isdigit():
+            N = int(N)
+        else: 
+            N = 10
+        mean_buy = 100
+        mean_sell = 100
+        std_buy = 30
+        std_sell = 30
+        buy_orders = (np.random.normal(size=(N,))*std_buy + mean_buy).astype(int)
+        sell_orders = (np.random.normal(size=(N,))*std_sell + mean_sell).astype(int)
 
-    book = OrderBook()
+        book = OrderBook()
 
-    # alternate buy, sell orders
-    for i in range(N):
-        n = np.random.randint(1,10)
-        print(f'placing buy order of (${buy_orders[i]},{n}). ')
-        book.bid(n,buy_orders[i])
-        n = np.random.randint(1,10)
-        print(f'placing sell order of (${sell_orders[i]},{n}). ')
-        book.ask(n,sell_orders[i])
+        # alternate buy, sell orders
+        for i in range(N):
+            n = np.random.randint(1,10)
+            print(f'placing buy order of (${buy_orders[i]},{n}). ')
+            book.bid(n,buy_orders[i])
+            n = np.random.randint(1,10)
+            print(f'placing sell order of (${sell_orders[i]},{n}). ')
+            book.ask(n,sell_orders[i])
         book.plot()

05b32d10ce85d37ccf6cd0e0fefead707a29bc11
Ben <picklenchips@icloud.com>
2024-05-08 01:17:02 -0700

    asdfs

--- 2006_AVELLANEDA_HFT.py
+++ 2006_AVELLANEDA_HFT.py
@@ -14,4 +14,27 @@ import numpy as np
     year = {2008},
     bdsk-url-1 = {https://doi.org/10.1080/14697680701381228}}
 """
-# asssume money market
+
+# asssume money market pays no interest
+# mid-price given by $dS_u = \sigma d W_u$
+"""
+ASSUMPTIONS:
+1. asssume money market pays no interest
+2. mid-price given by $dS_u = \sigma d W_u$
+- - initial value, S_t = s, W_t is standard 1D Brownian motion, 
+- - \sigma is constant
+3. agent can not affect drift or autocorrelation structure of stock
+4. 
+"""
+
+"""
+'frozen inventory' strategy
+inactive trader
+no limit orders and only holds inventory of q stocks until T
+"""
+
+def frozen_value(initial_wealth, stock_val, nstocks, time, 
+                 gamma, sigma, terminal_time):
+    first = -np.exp(-gamma*(initial_wealth+nstocks*stock_val))
+    second = np.exp((gamma*nstocks*sigma)**2 * (terminal_time - time) / 2)
+    return first * second

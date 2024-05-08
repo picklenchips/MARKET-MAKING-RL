60e4d099629ece45c09507733a98781b5f2bf903
Ben <picklenchips@icloud.com>
2024-05-08 02:53:38 -0700

    switchyswitchy

--- 2006_AVELLANEDA_HFT.py
+++ 2006_AVELLANEDA_HFT.py
@@ -21,7 +21,7 @@ import numpy as np
 ASSUMPTIONS:
 1. asssume money market pays no interest
 2. mid-price given by $dS_u = \sigma d W_u$
-- - initial value, S_t = s, W_t is standard 1D Brownian motion, 
+- - initial value, S_t = s, W_t is standard 1D Brownian motion,
 - - \sigma is constant
 3. agent can not affect drift or autocorrelation structure of stock
 4. 
@@ -40,7 +40,6 @@ value function given by v(x,s,q,t), where
  - x = initial wealth, q = nstocks, t = time
  - s = initial stock value, = midprice
 """
-
 
 
 gamma = sigma = 1; T = 1000
@@ -67,5 +66,33 @@ def res_price(s, q, t):  # reservation / indifference price
     return s - q * gamma * sigma**2 * (T-t)
 
 
+"""
+2.4 :  adding limit orders
+
+quotes bid price p^b, ask price p^a
+focus on distances \delta^b = s - p^b and \delta^a = p^a - s
+
+imagine market order of Q stocks arrives, the Q limit orders with lowest 
+ask prices are sold. if p^Q is price of highest limit order, 
+\Delta p = p^Q - s  is the temporary market impact of the trade
+
+ASSUME 
+- market buy orders "lift" agent's limit asks at Poisson rate
+\lambda^a(\delta^a), monotonically decreasing function
+- markey sells will "hit" the buy limit orders at rate \lambda^b
+these rates are also called the Poisson "intensities"
+
+X = wealth of agent. N_t^a is # stocks sold, $N_t^b$ is # stocks bought
+$dX_t = p^a dN_t^a - p^b dN_t^b$
+
+# stocks held at time $t$ is q_t
+objective is
+$u(s, x, q, t) = \max_{\delta^a,\delta^b} 
+\mathbb{E}_t \left[-\exp\left(-\gamma(X_T + q_T S_T)\right)\right]$
+"""
 
 
+"""
+model trading intensity.
+assume constant frequency f^Q(x)\alpha x^{-1-\alpha}
+"""

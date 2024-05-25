428969b1742174be423b2cf6088ffef03a5fdfc3
Ben <picklenchips@icloud.com>
2024-05-24 20:27:33 -0700

    m

--- market_2006.py
+++ market_2006.py
@@ -4,30 +4,6 @@ import torch
 import torch.nn as nn
 import tqdm as tqdm
 
-
-"""
-ASSUMPTIONS:
-1. asssume money market pays no interest
-2. mid-price given by $dS_u = \sigma d W_u$
-- - initial value, S_t = s, W_t is standard 1D Brownian motion,
-- - \sigma is constant
-3. agent can not affect drift or autocorrelation structure of stock
-4. 
-
-global:
- - gamma = discount price?
- - sigma = brownian motion variance
- - terminal time T at which stock dies or sumn
-
-'frozen inventory' strategy
-inactive trader
-no limit orders and only holds inventory of q stocks until T
-value function given by v(x,s,q,t), where
- - x = initial wealth, q = nstocks, t = time
- - s = initial stock value, = midprice
-"""
-
-
 gamma = sigma = 1; T = 1000
 # v(x,s,q,t), 
 # x = initial wealth, s = initial stock value, q = nstocks, t = time
@@ -36,13 +12,6 @@ def frozen_value(initial_wealth, stock_val, nstocks, time):
     second = np.exp((gamma*nstocks*sigma)**2 * (T - time) / 2)
     return first * second
 
-"""
-reservation_bid is price that makes agent indifferent to buy a stock
-v(x-r^b(s,q,t), s, q+1, t) >= v(x,s,q,t)
-reservation_ask is price that makes agent indifferent to sell a stock
-v(x+r^a(s,q,t), s, q-1, t) >= v(x,s,q,t)
-where r^b, r^a is bid, ask price
-"""
 def res_ask_price(s,q,t):
     return s + (1-2*q) * gamma * sigma**2 * (T-t)
 
@@ -321,6 +290,9 @@ def train_market():
 
     num_episodes = 1000
     with tqdm(total=num_episodes) as pbar:
+        # for epoch in range(epoches):
+
+        # ideally, create multiple trajectories 
         for episode in range(num_episodes):
             # set order book to zero and
             # move market forward 30 times
